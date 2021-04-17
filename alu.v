768f3ee7f73b96a9fd350b429351294f854124d1
Being97 <bjh9628@naver.com>
2021-04-17 20:34:43 +0900

    alu_overflow_flag

--- alu.v
+++ alu.v
@@ -8,7 +8,7 @@ module alu (A, B, func_code, branch_type, C, overflow_flag, bcond);
    input [3:0] func_code; //function code for the operation
    input [1:0] branch_type; //branch type for bne, beq, bgz, blz
    output reg [`NumBits-1:0] C; //output data C
-   output reg overflow_flag; 
+   output reg overflow_flag;
    output reg bcond; //1 if branch condition met, else 0
 
    //TODO: implement ALU
@@ -18,22 +18,50 @@ module alu (A, B, func_code, branch_type, C, overflow_flag, bcond);
       bcond = 0;
    end
 
-   always @(*) begin
+   always @(A or B or func_code or branch_type) begin
       case(func_code)
-         `FUNC_ADD: begin C = A + B; end
-			`FUNC_SUB: begin C = A - B; end
-			`FUNC_NOT: begin C = ~A; end
-			`FUNC_AND: begin C = A & B; end
-			`FUNC_ORR: begin C = A | B; end
-			`FUNC_TCP: begin C = ~A + 1; end
-			`FUNC_SHL: begin C = A << 1; end
-			`FUNC_SHR: begin C = $signed (A) >> 1; end
+         `FUNC_ADD: begin 
+            if A + B > `NumBits'b1111111111111111 begin overflow_flag = 1; end
+            else begin 
+               C = A + B;
+               overflow_flag = 0;
+               end
+			`FUNC_SUB: begin 
+            C = A - B;
+            overflow_flag = 0;
+            end
+			`FUNC_NOT: begin 
+            C = ~A;
+            overflow_flag = 0;
+            end
+			`FUNC_AND: begin 
+            C = A & B;
+            overflow_flag = 0;
+            end
+			`FUNC_ORR: begin 
+            C = A | B;
+            overflow_flag = 0;
+            end
+			`FUNC_TCP: begin 
+            if A == 0 begin overflow_flag = 1; end
+            else begin 
+               C = ~A + 1;
+               overflow_flag = 0;
+               end
+			`FUNC_SHL: begin 
+            C = A << 1;
+            overflow_flag = 0;
+            end
+			`FUNC_SHR: begin 
+            C = $signed (A) >> 1;
+            overflow_flag = 0;
+            end
       endcase
       case(branch_type)
          2'b00: begin bcond = A != B; end
          2'b01: begin bcond = A == B; end
          2'b10: begin bcond = A > 0; end
          2'b11: begin bcond = A < 0; end
+      endcase
    end
-
 endmodule

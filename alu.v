7383199e4d55a66e842c51e67935ac4b8395a7f3
Being97 <bjh9628@naver.com>
2021-04-17 20:04:34 +0900

    alu

--- alu.v
+++ alu.v
@@ -12,5 +12,28 @@ module alu (A, B, func_code, branch_type, C, overflow_flag, bcond);
    output reg bcond; //1 if branch condition met, else 0
 
    //TODO: implement ALU
-   
+   initial begin 
+      C = 0;
+      overflow_flag = 0;
+      bcond = 0;
+   end
+
+   always @(*) begin
+      case(func_code)
+         `FUNC_ADD: begin C = A + B; end
+			`FUNC_SUB: begin C = A - B; end
+			`FUNC_NOT: begin C = ~A; end
+			`FUNC_AND: begin C = A & B; end
+			`FUNC_ORR: begin C = A | B; end
+			`FUNC_TCP: begin C = ~A + 1; end
+			`FUNC_SHL: begin C = A << B; end
+			`FUNC_SHR: begin C = $signed (A) >> 1; end
+      endcase
+      case(branch_type)
+         2'b00: begin bcond = A != B; end
+         2'b01: begin bcond = A == B; end
+         2'b10: begin bcond = A > 0; end
+         2'b11: begin bcond = A < 0; end
+   end
+
 endmodule
